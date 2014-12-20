0e5522489abfb04bbd3e7f709a156fb0f6fd1b99
neokinok <daniel@neokinok.tv>
2014-12-20 22:30:54 +0100

    deleted:    css/mediabase.css 	deleted:    css/tabs.css 	deleted:    css/winxp.blue.css 	modified:   includes/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,12 +2,12 @@
 <tr><td style="padding:10px;" colspan="3">
 <center>
 <? if ($chn['footer_text']!="") { ?>
-<font size="1" style="font-family:arial,verdana;line-height:14px;color:#ccc">
+<font size="1" style="font-family:arial,verdana;line-height:14px;color:#999">
 <?=$chn['footer_text']?>
 </font>
 <? } ?>
-<font size="1" style="font-family:arial,verdana;line-height:14px;color:#ccc">
-<b>ExperimentalTV.org</b> Open Source Media Streaming by <a href="http://www.neokinok.tv" style="color:#ccc">Neokinok.tv</a> | (cc) <?=date('Y')?> Creative Commons.<br>
+<font size="1" style="font-family:arial,verdana;line-height:14px;color:#999">
+<b>ExperimentalTV.org</b> - nkMedia v.1 - Open Source Media Streaming by <a href="http://www.neokinok.tv" style="color:#ccc">Neokinok.tv</a> | (cc) <?=date('Y')?> Creative Commons.<br>
 </font></center>
 
 </td></tr></table>
